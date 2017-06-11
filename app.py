cfdad5610e13d1c4dfe7e973578756f640ce9315
eenateoh <eenateoh@users.noreply.github.com>
2017-06-11 14:25:07 +0800

    Added some stuff, hardcoded postpaid plans info,

--- app.py
+++ app.py
@@ -28,26 +28,53 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    if req.get("result").get("action") == "postpaid.details":
+        result = req.get("result"):
+        parameters = result.get("parameters")
+        plan_chosen = parameters.get("PostpaidPlan")
+        
+        cost = {'MaxisOne plan 188':188, 'MaxisOne plan 158':158, 'MaxisOne plan 128':128, 'MaxisOne plan 98':98}
+        data = {'MaxisOne plan 188':50, 'MaxisOne plan 158':40, 'MaxisOne plan 128':30, 'MaxisOne plan 98':20}
+        shareline = {'MaxisOne plan 188':4, 'MaxisOne plan 158':3, 'MaxisOne plan 128':1, 'MaxisOne plan 98':1}
+        
+        speech = "This plan is only " + str(cost[plan_chosen]) + "/month. " + 
+        "It comes with " + str(data[plan_chosen]) + "GB data and " + 
+        "Unlimited calls & SMS to all network." + 
+        "You can also get up to " + str(shareline[plan_chosen]) + "MaxisONE Share Line at RM 48/mth per line."
+        
+        print("Response":)
+        print(speech)
+        
+        return {
+            "speech": speech,
+            "displayText": speech,
+            #"data": {},
+            # "contextOut": [],
+            "source": "apiai-onlinestore-shipping"
+        }
+        
+        
+    if req.get("result").get("action") == "shipping.cost":
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+        cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+        speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
-    print("Response:")
-    print(speech)
+        print("Response:")
+        print(speech)
 
-    return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
-    }
+        return {
+            "speech": speech,
+            "displayText": speech,
+            #"data": {},
+            # "contextOut": [],
+            "source": "apiai-onlinestore-shipping"
+        }
+    
+     return {}
 
 
 if __name__ == '__main__':
